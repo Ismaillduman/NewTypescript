6203abadc367ea8dc6fcee3cf4e8ff4dc8efe20b
Issmailduman <https://github.com/Issmailduman>
2022-12-11 21:01:27 +0100

    private and public

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
 class Department {
     name:string;
+    private employees: string[]=[]
     constructor(n: string) {
         this.name=n;
+    }
+
+    desscribe(){
+        console.log('Department:' + this.name);
+        
+    }
+
+    addEmployee(employee: string){
+this.employees.push(employee);
+
+    }
+    printEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees);        
     }
 }
 
 
 const accouinting= new Department('Accounting');
 console.log(accouinting);
+accouinting.addEmployee('Snow');
+accouinting.addEmployee('Targaryen');
+accouinting.printEmployeeInformation();
 
-
-
-
+//accouinting.employees[2]='Anna';
+//public or private typescript supprt but JS NOT
