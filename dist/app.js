a2be711a589cf7a413d4a6a4207894cd17c24f06
Issmailduman <https://github.com/Issmailduman>
2022-12-11 20:13:59 +0100

    first commit

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const accouinting = new Department('Accounting');
+console.log(accouinting);
+//# sourceMappingURL=app.js.map
