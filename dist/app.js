e9a9e2f335f3ac831daa2be4b1e313e51303c538
Issmailduman <https://github.com/Issmailduman>
2022-12-11 20:42:21 +0100

    basic class

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,11 @@ class Department {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log('DEPARTMENT: ' + this.name);
+    }
 }
 const accouinting = new Department('Accounting');
 console.log(accouinting);
+accouinting.describe();
 //# sourceMappingURL=app.js.map
