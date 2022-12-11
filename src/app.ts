c077137d9179a11b77e632a3212f32a983eae884
Issmailduman <https://github.com/Issmailduman>
2022-12-11 20:14:27 +0100

    setup

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+class Department {
+    name:string;
+    constructor(n: string) {
+        this.name=n;
+    }
+}
+
+
+const accouinting= new Department('Accounting');
+console.log(accouinting);
+
+
+
+
