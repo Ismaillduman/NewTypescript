935401e9f3feb9437c43e95104e21d4a7da0cf40
Issmailduman <https://github.com/Issmailduman>
2022-12-11 21:01:53 +0100

    private and public

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,23 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
+    }
+    desscribe() {
+        console.log('Department:' + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 const accouinting = new Department('Accounting');
 console.log(accouinting);
+accouinting.addEmployee('Snow');
+accouinting.addEmployee('Targaryen');
+accouinting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
