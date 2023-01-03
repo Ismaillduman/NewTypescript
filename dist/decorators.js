2372d5c0f1ad51f410abd3cd13a01333e0ad0f4b
Issmailduman <https://github.com/Issmailduman>
2023-01-03 10:04:13 +0100

    decorators

--- dist/decorators.js
+++ dist/decorators.js
@@ -5,6 +5,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+function CarDecorator(constructor) {
+    constructor.prototype.date = new Date();
+    constructor.prototype.giveMeDate = function () {
+        alert(this.date);
+    };
+}
+function OtherDecorator(name) {
+    if (name === "Mike") {
+        return function (constructor) {
+            constructor.prototype.other = "other value";
+        };
+    }
+    else {
+        return null;
+    }
+}
+let Car = class Car {
+    constructor(brand) {
+        this.brand = brand;
+    }
+};
+Car = __decorate([
+    OtherDecorator("Mike"),
+    CarDecorator
+], Car);
+const car = new Car('Ford');
+console.log(car.brand);
+console.log(car.other);
 function PrintDecorator(target, propertyKey, descriptor) {
     console.log(target);
     console.log(propertyKey);
